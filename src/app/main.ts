2b1908bcaae1631803b61902c04b16959d7e07b6
Mara Adermann <maraadermann@maras-mbp.speedport_w_921v_1_44_000>
2018-06-17 20:21:05 +0200

    Running into "[Log] Error: exec proxy not found for :: PayPalMobile :: init (cordova.js, line 1003)" for payment

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,4 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+//for Payment
+import {enableProdMode} from '';
+enableProdMode();
+
 platformBrowserDynamic().bootstrapModule(AppModule);
