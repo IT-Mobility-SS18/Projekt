268c4370730bc27a5ff3a63590d1fad7e2baf4e7
Mara Adermann <maraadermann@Maras-MacBook-Pro.local>
2018-06-18 16:23:48 +0200

    Paypal finished

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 //for Payment
-import {enableProdMode} from '';
-enableProdMode();
+//import {enableProdMode} from '';
+//enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
