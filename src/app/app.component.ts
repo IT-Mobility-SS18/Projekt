bc886b0e28c95953ff09782b311069014aec4381
IT-Mobility-SS18 <maede.tala91@gmail.com>
2018-06-27 00:59:58 +0200

    Entwicklung des Warenkorbs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { StartPage } from '../pages/start/start';
+import { BasketPage } from '../pages/basket/basket';
 import { RegistrationPage } from '../pages/registration/registration';
 import { UserStartPage } from '../pages/user-start/user-start';
 import { OrderViewKitchenPage } from '../pages/orderViewKitchen/orderViewKitchen';
@@ -33,6 +34,7 @@ export class MyApp {
       { title: 'Login', component: StartPage },
       { title: 'Registrierung', component: RegistrationPage },
       { title: 'Startseite', component: UserStartPage },
+      { title: 'Warenkorb', component: BasketPage },
       { title: 'Bestellen', component: OrderCustomerPage },
       { title: 'Bestellungen', component: OrderViewCustomerPage },
       { title: 'Bestellübersicht Küche', component: OrderViewKitchenPage },
