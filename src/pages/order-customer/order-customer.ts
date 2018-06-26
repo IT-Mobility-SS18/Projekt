e2afed1c94f1ed47d0987262b74f526480fe55f3
Alex <alex@MBPfessional.fritz.box>
2018-06-26 09:19:48 +0200

    readded deleted code

--- src/pages/order-customer/order-customer.ts
+++ src/pages/order-customer/order-customer.ts
@@ -10,10 +10,24 @@ import { BasketPage } from '../basket/basket';
   templateUrl: 'order-customer.html',
 })
 export class OrderCustomerPage {
+  order: Order = {
+    UserId: undefined,
+    Name: undefined,
+    Price: undefined,
+    Quantity: undefined,
+    Picture: 'none',
+    DeliveryCosts: 0.0,
+    OrderState: 'open',
+    TimeStamp: 'timestampvalue',
+    RestaurantId: 45,
+    PayStatus: 'not paid',
+    Annotations: undefined,
+    TableId: 12
+  }
   public images: any;
    @ViewChild('slider') slider: Slides;
    page = 0;
-   constructor(public navCtrl: NavController) {
+   constructor(public navCtrl: NavController, public FirebaseService: FirebaseService) {
    }
 
    goToBasket() {
@@ -23,4 +37,7 @@ export class OrderCustomerPage {
    selectedTab(index) {
      this.slider.slideTo(index);
    }
+   addCustomerOrder(order: Order) {
+    this.FirebaseService.addCustomerOrder(order);
+  }
 }
