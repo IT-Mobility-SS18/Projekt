0fec0e8650fd95daacf5b54bc9f380f80678b34b
Irem Cömert <iremc@live.de>
2018-06-30 17:29:40 +0200

    Tabs für Bestellstatus "Open", "Ready" und "Done"

--- src/pages/order-view-service/order-view-service.ts
+++ src/pages/order-view-service/order-view-service.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams} from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, IonicPage, Slides, AlertController} from 'ionic-angular';
 import { FirebaseService } from './../../providers/firebase/firebase-service';
+import { Order } from '../../models/order/order.model'
 import { BasketPage } from '../basket/basket';
+import { AngularFireAuth } from 'angularfire2/auth';
+import firebase, { storage } from "firebase";
 
 
 @Component({
@@ -10,6 +13,10 @@ import { BasketPage } from '../basket/basket';
 })
 
 export class OrderViewServicePage {
+  @ViewChild('slider') slider: Slides;
+  selectedSegment: string;
+  slides: any;
+   page = 0;
 
   ListCategory = [];
   viewarr= [];
@@ -21,6 +28,11 @@ export class OrderViewServicePage {
       console.log(this.viewarr);
     })
   }
+
+  selectedTab(index) {
+       this.slider.slideTo(index);
+     }
+
   goToBasket(){
     this.navCtrl.push(BasketPage);
   }
