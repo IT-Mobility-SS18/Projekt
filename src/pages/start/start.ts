49897276a5cb50683541b5931901d715d29fc985
Alex <alex@MBPfessional.fritz.box>
2018-08-13 11:07:20 +0200

    Translation + changed to Toast type messages for login

--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController, NavParams, AlertController} from 'ionic-angular';
+import { NavController, NavParams, AlertController, ToastController} from 'ionic-angular';
 import { RegistrationPage } from '../registration/registration';
 import { UserStartPage } from '../user-start/user-start';
 import { AngularFireAuth } from 'angularfire2/auth';
@@ -16,7 +16,14 @@ import { MenuController } from 'ionic-angular'; //Side-Menu löschen
 export class StartPage {
   @ViewChild('username') user;
   @ViewChild('password') password;
-  constructor(private menu: MenuController, public navCtrl: NavController,private alertCtrl: AlertController, private fire:AngularFireAuth,public navParams: NavParams, public FirebaseService: FirebaseService){
+  MyErrorMessage;
+  constructor(private menu: MenuController, 
+    public navCtrl: NavController,
+    private alertCtrl: AlertController, 
+    private fire:AngularFireAuth,
+    public navParams: NavParams, 
+    public FirebaseService: FirebaseService,
+    private toastCtrl: ToastController){
     
   }
 
@@ -37,12 +44,23 @@ alert(message: string) {
   }).present();
 }
 
+presentToast(ExtMessage, ExtDur) {
+  let toast = this.toastCtrl.create({
+    message: ExtMessage,
+    duration: ExtDur,
+    position: 'bottom',
+    cssClass: 'toast-container'
+  });
+  toast.present();
+}
+
 // login
 userStart(){
   this.fire.auth.signInWithEmailAndPassword(this.user.value, this.password.value)
   .then( data => {
     console.log('Login data passed', this.fire.auth.currentUser);
-    this.alert('Login erfolgreich.');
+    //this.alert('Login erfolgreich.');
+    this.presentToast("Login erfolgreich.", 1000);
     //wichtig für Anzeige der Variablen in der HTML da diese erst vorliegen müssen
     this.FirebaseService.CurrentUserFirstName=this.FirebaseService.getCurrentUserFirstName(this.fire.auth.currentUser.uid).then(()=>{ 
     this.navCtrl.setRoot( UserStartPage )
@@ -50,7 +68,8 @@ userStart(){
   })
   .catch( error => {
     console.log('Error during login', error);
-    this.alert(error.message);
+    this.MyErrorMessage = "E-Mail-Adresse oder Passwort nicht korrekt.";
+    this.presentToast(this.MyErrorMessage, 2000);
   })
   console.log('User signed in: ', this.user.value);
 }
